71e564f81658992712b013dee3fa28deece1b825
Joe Black <jrb00013wvu@gmail.com>
2026-05-20 17:57:42 -0400

    feat: implement physics-driven AMistspirePhysicalButton for tactile VR world interaction

---
+++ game/Source/Mistspire/Private/MistspirePhysicalButton.cpp
@@ -0,0 +1,54 @@
+#include "MistspirePhysicalButton.h"
+#include "Components/StaticMeshComponent.h"
+#include "PhysicsEngine/PhysicsConstraintComponent.h"
+
+AMistspirePhysicalButton::AMistspirePhysicalButton()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	BaseMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BaseMesh"));
+	SetRootComponent(BaseMesh);
+
+	ButtonMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ButtonMesh"));
+	ButtonMesh->SetupAttachment(BaseMesh);
+	ButtonMesh->SetSimulatePhysics(true);
+	ButtonMesh->SetLinearDamping(5.0f);
+	ButtonMesh->SetAngularDamping(5.0f);
+
+	PhysicsConstraint = CreateDefaultSubobject<UPhysicsConstraintComponent>(TEXT("PhysicsConstraint"));
+	PhysicsConstraint->SetupAttachment(BaseMesh);
+}
+
+void AMistspirePhysicalButton::BeginPlay()
+{
+	Super::BeginPlay();
+
+	InitialLocalLocation = ButtonMesh->GetRelativeLocation();
+
+	// Configure physics constraint for linear movement on Z axis only
+	PhysicsConstraint->SetConstrainedComponents(BaseMesh, NAME_None, ButtonMesh, NAME_None);
+	PhysicsConstraint->SetLinearXLimit(ELinearConstraintMotion::LCM_Locked, 0);
+	PhysicsConstraint->SetLinearYLimit(ELinearConstraintMotion::LCM_Locked, 0);
+	PhysicsConstraint->SetLinearZLimit(ELinearConstraintMotion::LCM_Limited, 5.0f); // 5cm travel
+	
+	// Spring to return button
+	PhysicsConstraint->SetLinearDriveParams(500.0f, 50.0f, 0.0f);
+	PhysicsConstraint->SetLinearZDrive(true);
+}
+
+void AMistspirePhysicalButton::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	float CurrentOffset = (ButtonMesh->GetRelativeLocation() - InitialLocalLocation).Size();
+
+	if (CurrentOffset > ActivationThresholdCm && !bIsPressed)
+	{
+		bIsPressed = true;
+		OnButtonPressed.Broadcast();
+	}
+	else if (CurrentOffset < ActivationThresholdCm * 0.5f && bIsPressed)
+	{
+		bIsPressed = false;
+	}
+}
---
+++ game/Source/Mistspire/Public/MistspirePhysicalButton.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MistspirePhysicalButton.generated.h"
+
+class UStaticMeshComponent;
+class UPhysicsConstraintComponent;
+
+UCLASS()
+class MISTSPIRE_API AMistspirePhysicalButton : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AMistspirePhysicalButton();
+
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnButtonPressed);
+	UPROPERTY(BlueprintAssignable, Category = "Mistspire|Interaction")
+	FOnButtonPressed OnButtonPressed;
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Mistspire|Components")
+	TObjectPtr<UStaticMeshComponent> BaseMesh;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Mistspire|Components")
+	TObjectPtr<UStaticMeshComponent> ButtonMesh;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Mistspire|Components")
+	TObjectPtr<UPhysicsConstraintComponent> PhysicsConstraint;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mistspire|Interaction")
+	float ActivationThresholdCm = 2.0f;
+
+private:
+	bool bIsPressed = false;
+	FVector InitialLocalLocation;
+};
