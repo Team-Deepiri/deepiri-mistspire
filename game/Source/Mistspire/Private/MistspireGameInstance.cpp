ae77121be9d36957c973a367ff7b135320a7d1a5
Joe Black <jrb00013wvu@gmail.com>
2026-05-20 16:42:38 -0400

    feat: implement session hosting, discovery, and joining logic in MistspireGameInstance

---
+++ game/Source/Mistspire/Private/MistspireGameInstance.cpp
@@ -0,0 +1,120 @@
+#include "MistspireGameInstance.h"
+#include "OnlineSubsystem.h"
+#include "OnlineSessionSettings.h"
+#include "Kismet/GameplayStatics.h"
+
+UMistspireGameInstance::UMistspireGameInstance()
+{
+	CreateSessionCompleteDelegate = FOnCreateSessionCompleteDelegate::CreateUObject(this, &UMistspireGameInstance::OnCreateSessionComplete);
+	FindSessionsCompleteDelegate = FOnFindSessionsCompleteDelegate::CreateUObject(this, &UMistspireGameInstance::OnFindSessionsComplete);
+	JoinSessionCompleteDelegate = FOnJoinSessionCompleteDelegate::CreateUObject(this, &UMistspireGameInstance::OnJoinSessionComplete);
+}
+
+void UMistspireGameInstance::Init()
+{
+	Super::Init();
+}
+
+void UMistspireGameInstance::HostSession(bool bIsLAN, int32 MaxPlayers)
+{
+	IOnlineSubsystem* Subsystem = IOnlineSubsystem::Get();
+	if (Subsystem)
+	{
+		IOnlineSessionPtr SessionInterface = Subsystem->GetSessionInterface();
+		if (SessionInterface.IsValid())
+		{
+			FOnlineSessionSettings SessionSettings;
+			SessionSettings.bIsLANMatch = bIsLAN;
+			SessionSettings.NumPublicConnections = MaxPlayers;
+			SessionSettings.bAllowJoinInProgress = true;
+			SessionSettings.bAllowJoinViaPresence = true;
+			SessionSettings.bShouldAdvertise = true;
+			SessionSettings.bUsesPresence = true;
+			SessionSettings.bUseLobbiesIfAvailable = true;
+
+			SessionInterface->AddOnCreateSessionCompleteDelegate_Handle(CreateSessionCompleteDelegate);
+			SessionInterface->CreateSession(0, NAME_GameSession, SessionSettings);
+		}
+	}
+}
+
+void UMistspireGameInstance::OnCreateSessionComplete(FName SessionName, bool bWasSuccessful)
+{
+	if (bWasSuccessful)
+	{
+		UWorld* World = GetWorld();
+		if (World)
+		{
+			UGameplayStatics::OpenLevel(World, TEXT("/Game/Maps/Main_WP"), true, TEXT("listen"));
+		}
+	}
+}
+
+void UMistspireGameInstance::FindSessions()
+{
+	IOnlineSubsystem* Subsystem = IOnlineSubsystem::Get();
+	if (Subsystem)
+	{
+		IOnlineSessionPtr SessionInterface = Subsystem->GetSessionInterface();
+		if (SessionInterface.IsValid())
+		{
+			SessionSearch = MakeShareable(new FOnlineSessionSearch());
+			SessionSearch->bIsLanQuery = true; // For LAN discovery like Minecraft
+			SessionSearch->MaxSearchResults = 20;
+			SessionSearch->QuerySettings.Set(SEARCH_PRESENCE, true, EOnlineComparisonOp::Equals);
+
+			SessionInterface->AddOnFindSessionsCompleteDelegate_Handle(FindSessionsCompleteDelegate);
+			SessionInterface->FindSessions(0, SessionSearch.ToSharedRef());
+		}
+	}
+}
+
+void UMistspireGameInstance::OnFindSessionsComplete(bool bWasSuccessful)
+{
+	if (bWasSuccessful && SessionSearch.IsValid())
+	{
+		// In a real UI we would populate a list, for now we join the first found
+		if (SessionSearch->SearchResults.Num() > 0)
+		{
+			JoinSession(0);
+		}
+	}
+}
+
+void UMistspireGameInstance::JoinSession(int32 SessionIndex)
+{
+	IOnlineSubsystem* Subsystem = IOnlineSubsystem::Get();
+	if (Subsystem)
+	{
+		IOnlineSessionPtr SessionInterface = Subsystem->GetSessionInterface();
+		if (SessionInterface.IsValid() && SessionSearch.IsValid())
+		{
+			SessionInterface->AddOnJoinSessionCompleteDelegate_Handle(JoinSessionCompleteDelegate);
+			SessionInterface->JoinSession(0, NAME_GameSession, SessionSearch->SearchResults[SessionIndex]);
+		}
+	}
+}
+
+void UMistspireGameInstance::OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result)
+{
+	if (Result == EOnJoinSessionCompleteResult::Success)
+	{
+		IOnlineSubsystem* Subsystem = IOnlineSubsystem::Get();
+		if (Subsystem)
+		{
+			IOnlineSessionPtr SessionInterface = Subsystem->GetSessionInterface();
+			if (SessionInterface.IsValid())
+			{
+				FString ConnectionInfo;
+				if (SessionInterface->GetResolvedConnectString(SessionName, ConnectionInfo))
+				{
+					APlayerController* PC = GetFirstLocalPlayerController();
+					if (PC)
+					{
+						PC->ClientTravel(ConnectionInfo, ETravelType::TRAVEL_Absolute);
+					}
+				}
+			}
+		}
+	}
+}
