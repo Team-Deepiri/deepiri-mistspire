58cccdb28207772500977304344206b60765f6fe
Joe Black <jrb00013wvu@gmail.com>
2026-05-20 16:42:21 -0400

    feat: integrate OnlineSubsystemSteam and initialize GameInstance for session management

--- game/Source/Mistspire/Mistspire.Build.cs
+++ game/Source/Mistspire/Mistspire.Build.cs
@@ -9,6 +9,8 @@ public class Mistspire : ModuleRules
 		{
 			"Core", "CoreUObject", "Engine", "InputCore",
 			"HeadMountedDisplay", "EnhancedInput", "MistspireOpenXRNative",
+			"OnlineSubsystem", "OnlineSubsystemUtils", "OnlineSubsystemSteam",
+			"PhysicsCore",
 		});
 
 		PrivateDependencyModuleNames.AddRange(new string[]
---
+++ game/Source/Mistspire/Public/MistspireGameInstance.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "Interfaces/OnlineSessionInterface.h"
+#include "MistspireGameInstance.generated.h"
+
+UCLASS()
+class MISTSPIRE_API UMistspireGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+
+public:
+	UMistspireGameInstance();
+
+	virtual void Init() override;
+
+	/** Starts a hosting session. bIsLAN determines if it's visible on the local network. */
+	UFUNCTION(BlueprintCallable, Category = "Mistspire|Multiplayer")
+	void HostSession(bool bIsLAN = false, int32 MaxPlayers = 4);
+
+	/** Starts searching for available sessions. */
+	UFUNCTION(BlueprintCallable, Category = "Mistspire|Multiplayer")
+	void FindSessions();
+
+	/** Joins a specific session found via FindSessions. */
+	void JoinSession(int32 SessionIndex);
+
+protected:
+	void OnCreateSessionComplete(FName SessionName, bool bWasSuccessful);
+	void OnFindSessionsComplete(bool bWasSuccessful);
+	void OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result);
+
+private:
+	TSharedPtr<class FOnlineSessionSearch> SessionSearch;
+	FOnCreateSessionCompleteDelegate CreateSessionCompleteDelegate;
+	FOnFindSessionsCompleteDelegate FindSessionsCompleteDelegate;
+	FOnJoinSessionCompleteDelegate JoinSessionCompleteDelegate;
+};
